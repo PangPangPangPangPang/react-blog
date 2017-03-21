7abbe3b61c994086328e0ae51f67cdfb8a130566
wangyefeng <wangyefeng@baidu.com>
2017-03-21 16:36:04 +0800

    update

--- src/container/article/article.js
+++ src/container/article/article.js
@@ -10,45 +10,6 @@ import './article.css'
 
 const renderer = new marked.Renderer()
 
-renderer.heading = function heading(text, level) {
-  let content = ''
-  if (level === 1) {
-    content = `${text}<br/><br/>`
-  } else {
-    content = `<br/>${text}<br/><br/>`
-  }
-  const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-')
-  return `<h${level}><a name="${escapedText}" class="anchor" href="#${escapedText}"><span class="header-link"></span></a>${content}</h${level}>`
-}
-
-renderer.list = function list(body){
-return `<div class="article-list" >${body}</div>`
-}
-
-// renderer.list = function list(body, ordered) {
-  // const type = ordered ? 'ol' : 'ul'
-  // const attrs = {
-    // type: '',
-    // start: '',
-  // }
-
-  // if (+ordered || +ordered === 0) {
-    // if (+ordered !== 1) {
-      // attrs.start = `start="${ordered}"`
-    // }
-  // } else {
-    // const typeCase = (ordered === ordered.toUpperCase()) ? 'A' : 'a'
-    // attrs.type = `type="${typeCase}"`
-    // const startLetter = ordered.toString().toUpperCase().charCodeAt(0) - 64
-    // attrs.start = (startLetter > 1 && startLetter < 9) ? `start="${startLetter}"` : ''
-  // }
-  // const attr = [attrs.type, attrs.start].filter(x => x).join(' ')
-
-  // return `<${type} ${attr}>
-    // ${body}
-    // </${type}>
-    // `
-// }
 marked.setOptions({
   highlight(code) {
     return `<div class="article-code" >${hl.highlightAuto(code).value}</div>`
@@ -62,6 +23,30 @@ marked.setOptions({
   smartLists: true,
   smartypants: true,
 })
+
+renderer.heading = function heading(text, level) {
+  const font = 24 - (level * 2)
+  return `<div style="font-size: ${font}px;margin-bottom: 10px"><strong>${text}</strong></div>`
+}
+
+renderer.paragraph = function paragraph(text) {
+  return `<div class="article-paragraph">${text}</div>`
+}
+
+renderer.list = function list(body, ordered) {
+  const type = ordered ? 'lower-roman' : 'circle'
+  return `<ul style="list-style-type: ${type}; margin-left: 20px; font-size: 15px; margin-bottom: 20px;">${body}</ul>`
+}
+
+renderer.listitem = function listitem(text) {
+  return `<li>${text}</li>`
+}
+
+renderer.blockquote = function em(text) {
+  console.log('//////')
+  console.log(text)
+  return `<blockquote>${text}</blockquote>`
+}
 
 class Article extends React.Component {
   constructor(props) {
