41a4efc069b6fd7a78249488429bd9bb6c97603f
PangPangPangPangPang <446964321@qq.com>
2017-05-21 21:20:29 +0800

    update

--- src/container/tag/tag.js
+++ src/container/tag/tag.js
@@ -8,32 +8,61 @@ class Tag extends React.Component {
   constructor() {
     super()
     this.ws = new WebSocket('ws://localhost:8000/chat')
+    this.state = {
+      userName: '',
+      toName: '',
+    }
   }
   componentDidMount() {
     this.ws.onopen = () => {
-      console.log('open')
     }
-    this.ws.onmessage = (obj) => {
-      console.log(obj)
+    this.ws.onmessage = () => {
     }
     this.ws.onclose = () => {
-      console.log('close')
-
     }
-    this.ws.onerror = (error) => {
-      console.log(error)
+    this.ws.onerror = () => {
     }
   }
+  handleChange = (e) => {
+    this.setState({
+      userName: e.target.value,
+    })
+  }
+  handleToChange = (e) => {
+    this.setState({
+      toName: e.target.value,
+    })
+  }
   clickButton = () => {
-    this.ws.send('wokao')
+    const a = {
+      action: 'normal',
+      from: this.state.userName,
+      to: this.state.toName,
+      message: 'Hello!',
+    }
+    this.ws.send(JSON.stringify(a))
+  }
+  clickRegister = () => {
+    const a = {
+      action: 'register',
+      from: this.state.userName,
+      to: this.state.toName,
+    }
+    this.ws.send(JSON.stringify(a))
   }
   render() {
     return (
-      // <SocketProvider socket={this.socket}>
-        <div >
-          <button className="tag-button" onClick={this.clickButton} />
-        </div>
-      // </SocketProvider>
+      <div>
+        <form onSubmit={this.clickRegister}>
+          UserName:
+          <input type="text" value={this.state.userName} onChange={this.handleChange} />
+          <input type="text" value={this.state.toName} onChange={this.handleToChange} />
+          <button type="submit">Submit</button>
+        </form>
+        <button onClick={this.clickButton}>
+          Send Message!
+        </button>
+      </div>
     )
   }
 
